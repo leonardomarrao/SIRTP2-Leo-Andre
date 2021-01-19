fb69118515985a02c804e798ca95a9af231c7804
leonardomarrao <61155148+leonardomarrao@users.noreply.github.com>
2021-01-19 23:32:50 +0000

    rodrigoCorrigiuAindaNaoTestei

--- frontend/my-app/src/components/CartaProduto.vue
+++ frontend/my-app/src/components/CartaProduto.vue
@@ -1,6 +1,6 @@
-<!--<template>
+<template>
   <div class="card">
-    <img src={{ imagem }} />
+    <img :src="imagem" />
     <div class="container">
       <h5>{{ nome }}</h5>
       <h5>{{ preco }}</h5>
@@ -8,15 +8,9 @@
     </div>
   </div>
 </template>
--->
+
 <script>
-Vue.component('blog-post', { props: ['imagem', 'nome', 'preco'], template:
-'<div class="card">
-    <img src={{ imagem }} />
-    <div class="container">
-      <h5>{{ nome }}</h5>
-      <h5>{{ preco }}</h5>
-      <a></a>
-    </div>
-  </div>' })
+export default {
+  props: ["nome", "preco", "imagem"],
+}
 </script>
